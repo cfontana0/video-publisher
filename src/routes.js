da841c9fe999f0f483b3741e82275f437fd21556
cfontana0 <cfontana0@gmail.com>
2017-07-04 01:00:52 +0200

    Minor fixes

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import IndexRoute from 'react-router/lib/IndexRoute'
 import Route from 'react-router/lib/Route'
 import App from './components/App'
 import Home from './components/Home'
+import Videos from './components/Videos'
 
 export default (
     <Route path='/' component={App} onUpdate={() => window.scrollTo(0, 0)}>
         <IndexRoute component={Home} />
+        <Route path='videos' component={Videos} />
     </Route>
 )
